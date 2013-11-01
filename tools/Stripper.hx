0b2fae4984b513b9080d5969f600c568484e6ba6
Joshua Granick <joshua@joshuagranick.com>
2013-11-01 00:16:03 -0700

    Begin working on updated tools

---
+++ tools/Stripper.hx
@@ -0,0 +1,25 @@
+
+class Stripper
+{
+   public var mExe:String;
+   public var mFlags : Array<String>;
+
+   public function new(inExe:String)
+   {
+      mFlags = [];
+      mExe = inExe;
+   }
+   public function strip(inTarget:String)
+   {
+      var args = new Array<String>();
+
+      args = args.concat(mFlags);
+
+      args.push(inTarget);
+
+      Sys.println( mExe + " " + args.join(" ") );
+      var result = BuildTool.runCommand( mExe, args );
+      if (result!=0)
+         throw "Error : " + result + " - build cancelled";
+   }
+}
